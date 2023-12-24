b15ac2ae541271b42339f4adfdfaae9f6e339f5d
nenshie <dukicn01@gmail.com>
2023-12-24 17:36:11 +0100

    dodata migracija za brisanje tabele sluzbenik

---
+++ ssLaravel/database/migrations/2023_12_24_163418_create_sluzbeniks_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSluzbeniksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sluzbeniks', function (Blueprint $table) {
+            $table->id();
+            $table->string('ime');
+            $table->string('prezime');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('jmbg');
+            $table->string('adresa');
+            $table->string('kontakt');
+
+            $table->string('pozicija');
+            $table->date('datumpocetkarada');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sluzbeniks');
+    }
+}
