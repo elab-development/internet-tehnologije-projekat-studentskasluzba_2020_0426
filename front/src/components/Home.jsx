2dbd4326a5ea5c67d02fdb39eb9d9e2fb875a053
nenshie <dukicn01@gmail.com>
2024-01-07 23:17:42 +0100

    kreirana pocetna stranica

---
+++ front/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'; 
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://fon.bg.ac.rs/wp-content/uploads/2023/10/FON-Logo.svg" alt="Logo FONA" className="Fon-logo" />
+        <h1>Dobrodošli na eStudent servis FONA</h1>
+        <p>
+          Fakultet organizacionih nauka je jedan od vodećih fakulteta u regionu, 
+          posvećen pružanju kvalitetnog obrazovanja i inovativnog istraživanja.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
