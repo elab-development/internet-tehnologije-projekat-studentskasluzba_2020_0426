2681382abc1076829a49bf4e3f71fe0d1fc30060
nenshie <dukicn01@gmail.com>
2024-01-07 23:24:12 +0100

    dodad navbar

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
+ 
 import './App.css';
 import Home from './components/Home';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className="App">
-       <Home></Home>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />  
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+        </Routes>
+      </div>
+  </Router>
   );
 }
 
---
+++ front/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+ 
+import './Navbar.css';
+
+function Navbar() {
+  const navigate = useNavigate();
+
+  return (
+    <nav className="navbar">
+      <img src="https://fon.bg.ac.rs/wp-content/uploads/2023/10/FON-Logo.svg" alt="Logo FONA" className="navbar-logo" />
+      <ul className="navbar-nav">
+        <li className="nav-item" onClick={() => navigate('/login')}>
+          Login
+        </li>
+      
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
