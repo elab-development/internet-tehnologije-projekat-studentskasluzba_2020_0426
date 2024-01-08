7b9f4d17e7246515f6190fc90a02efbd3a88162c
MilicaV10 <milica.vukovljak001@gmail.com>
2024-01-08 14:28:29 +0100

    dodato obracunavanje prosecne ocene i ukupnog broja esp poena

--- front/src/components/Ispiti.jsx
+++ front/src/components/Ispiti.jsx
@@ -4,32 +4,42 @@ import IspitRed from './IspitRed';
 import './Ispiti.css';
 
 function Ispiti() {
+  
   const [ispiti, setIspiti] = useState([]);
-  const [pagination, setPagination] = useState({});
+  const [ukupnoESP, setUkupnoESP] = useState(0);
+  const [prosecnaOcena, setProsecnaOcena] = useState(0);
 
   useEffect(() => {
     const fetchIspiti = async () => {
-      const authId = sessionStorage.getItem('auth_id');
+      const authId = sessionStorage.getItem('auth_id'); //  ID ulogovanog studenta
       try {
         const response = await axios.get('http://127.0.0.1:8000/api/ispiti', {
           params: { 
-            auth_id: authId,
+            auth_id: authId, 
           },
-        
         });
-        console.log(response.data.data)
-        setIspiti(response.data.data);
-        setPagination({
-          prev: response.data.prev_page_url,
-          next: response.data.next_page_url,
-        });
+        // Filtriramo ispite tako da prikažemo samo one koji pripadaju ulogovanom studentu
+        const filteredIspiti = response.data.data.filter(ispit => ispit.student.id.toString() === authId);
+        setIspiti(filteredIspiti); 
+
+        //uzimamo samo polozene ispite
+        const validIspiti = response.data.data.filter(ispit => ispit.ocena > 5);
+        setIspiti(validIspiti);
+
+        // Izračunavamo ukupno ESP i prosečnu ocenu
+        const totalESP = validIspiti.reduce((sum, ispit) => sum + ispit.predmet.esbp, 0);
+        setUkupnoESP(totalESP);
+
+        const averageGrade = validIspiti.reduce((sum, ispit) => sum + ispit.ocena, 0) / validIspiti.length;
+        setProsecnaOcena(averageGrade.toFixed(2)); 
       } catch (error) {
         console.error('Error fetching ispiti', error);
       }
     };
-
+  
     fetchIspiti();
   }, []);
+  
 
   return (
     <div className="ispiti-container">
@@ -50,7 +60,10 @@ function Ispiti() {
           ))}
         </tbody>
       </table>
-     
+      <div className="ispiti-summary">
+          <p>Ukupan broj ESP bodova: {ukupnoESP}</p>
+          <p>Prosečna ocena: {prosecnaOcena}</p>
+        </div>
     </div>
   );
 }
