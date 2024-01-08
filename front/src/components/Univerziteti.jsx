cb0d11ecd61fefe391ce3878800d40ce8421fd38
MilicaV10 <milica.vukovljak001@gmail.com>
2024-01-08 14:55:10 +0100

    dodata paginacija na stranicu za prikaz univerziteta

--- front/src/components/Univerziteti.jsx
+++ front/src/components/Univerziteti.jsx
@@ -3,14 +3,16 @@ import axios from 'axios';
 import './Univerziteti.css';
 
 function Univerziteti() {
-  const [univerziteti, setUniverziteti] = useState([]);
+    const [univerziteti, setUniverziteti] = useState([]);
+    const [trenutnaStranica, setTrenutnaStranica] = useState(1);
+    const [univerzitetaPoStranici] = useState(9);
+    const ukupnoStranica = Math.ceil(univerziteti.length / univerzitetaPoStranici);
 
   useEffect(() => {
     const fetchUniverziteti = async () => {
       try {
         const response = await axios.get('http://universities.hipolabs.com/search');
         setUniverziteti(response.data);
-        console.log(response.data)
       } catch (error) {
         console.error('Greška prilikom dohvatanja podataka o univerzitetima', error);
       }
@@ -19,16 +21,42 @@ function Univerziteti() {
     fetchUniverziteti();
   }, []);
 
+    const trenutniUniverziteti = univerziteti.slice(
+        (trenutnaStranica - 1) * univerzitetaPoStranici,
+        trenutnaStranica * univerzitetaPoStranici
+    );
+
+    const promeniStranicu = brojStranice => {
+        setTrenutnaStranica(brojStranice);
+    };
+
   return (
+    <div style={{margin:"7%"}}>
+     <div className="paginacija">
+        <button onClick={() => promeniStranicu(1)} disabled={trenutnaStranica === 1}>Prva</button>
+        <button onClick={() => promeniStranicu(trenutnaStranica - 1)} disabled={trenutnaStranica === 1}>Prethodna</button>
+        <button onClick={() => promeniStranicu(trenutnaStranica + 1)} disabled={trenutnaStranica === ukupnoStranica}>Sledeća</button>
+        <button onClick={() => promeniStranicu(ukupnoStranica)} disabled={trenutnaStranica === ukupnoStranica}>Poslednja</button>
+      </div>
+   
     <div className="univerziteti-container">
-      {univerziteti.map((uni, index) => (
+        
+      {trenutniUniverziteti.map((uni, index) => (
         <div key={index} className="univerzitet">
           <h2>{uni.name}</h2>
           <p>{uni.country}</p>
           <a href={`http://${uni.web_pages[0]}`} target="_blank" rel="noreferrer">Posetite sajt</a>
         </div>
       ))}
-    </div>
+       </div>
+      <div className="paginacija">
+        <button onClick={() => promeniStranicu(1)} disabled={trenutnaStranica === 1}>Prva</button>
+        <button onClick={() => promeniStranicu(trenutnaStranica - 1)} disabled={trenutnaStranica === 1}>Prethodna</button>
+        <button onClick={() => promeniStranicu(trenutnaStranica + 1)} disabled={trenutnaStranica === ukupnoStranica}>Sledeća</button>
+        <button onClick={() => promeniStranicu(ukupnoStranica)} disabled={trenutnaStranica === ukupnoStranica}>Poslednja</button>
+      </div>
+   
+      </div>
   );
 }
 
