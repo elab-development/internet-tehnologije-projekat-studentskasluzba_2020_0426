387d0034573dc84085833338737db9375fb3a09f
dukicn01@gmail.com <nevenajankovic00108@gmail.com>
2024-01-10 01:13:55 +0100

    dodati filteri za prikaz univerziteta po drzavama

--- front/src/components/Univerziteti.jsx
+++ front/src/components/Univerziteti.jsx
@@ -7,7 +7,7 @@ function Univerziteti() {
     const [trenutnaStranica, setTrenutnaStranica] = useState(1);
     const [univerzitetaPoStranici] = useState(9);
     const ukupnoStranica = Math.ceil(univerziteti.length / univerzitetaPoStranici);
-
+    const [trenutnaDrzava, setTrenutnaDrzava] = useState('');
   useEffect(() => {
     const fetchUniverziteti = async () => {
       try {
@@ -29,9 +29,20 @@ function Univerziteti() {
     const promeniStranicu = brojStranice => {
         setTrenutnaStranica(brojStranice);
     };
+    const filtriraniUniverziteti = trenutnaDrzava 
+    ? trenutniUniverziteti.filter(uni => uni.country === trenutnaDrzava)
+    : univerziteti;
+ 
 
   return (
     <div style={{margin:"7%"}}>
+      <div className="filteri">
+                <button onClick={() => setTrenutnaDrzava('France')}>France</button>
+                <button onClick={() => setTrenutnaDrzava('Chile')}>Chile</button>
+                <button onClick={() => setTrenutnaDrzava('Ukraine')}>Ukraine</button>
+                <button onClick={() => setTrenutnaDrzava('United Kingdom')}>United Kingdom</button>
+                <button onClick={() => setTrenutnaDrzava('')}>Svi Univerziteti</button>
+            </div>
      <div className="paginacija">
         <button onClick={() => promeniStranicu(1)} disabled={trenutnaStranica === 1}>Prva</button>
         <button onClick={() => promeniStranicu(trenutnaStranica - 1)} disabled={trenutnaStranica === 1}>Prethodna</button>
@@ -41,7 +52,7 @@ function Univerziteti() {
    
     <div className="univerziteti-container">
         
-      {trenutniUniverziteti.map((uni, index) => (
+      {filtriraniUniverziteti.map((uni, index) => (
         <div key={index} className="univerzitet">
           <h2>{uni.name}</h2>
           <p>{uni.country}</p>
